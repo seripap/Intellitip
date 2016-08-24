103493c84fc17351525bbc3b87d1d4f889043676
Tomasz Górecki <tomasz.gorecki@ts.fujitsu.com>
2016-08-24 11:05:32 +0200

    Further code reorganization & cleanup

--- intellitip.py
+++ intellitip.py
@@ -11,22 +11,9 @@ import sublime
 import sublime_plugin
 
 
-def load_css_file_to_settings(settings):
-    css_file = "Packages/" + settings.get("css_file", "Intellitip/css/default.css")
-
-    css = sublime.load_resource(css_file).replace("\r", "")
-
-    settings.set("css", css)
-
-
-def get_function_names(view):
-    return [view.substr(view.word(view.sel()[0]))]
-
-
 class IntellitipCommand(sublime_plugin.TextCommand):
-    cache    = {}
-    lang     = None
-    settings = None
+    cached_completions = {}
+    settings           = None
 
     def __init__(self, view):
         super(IntellitipCommand, self).__init__(view)
@@ -35,106 +22,104 @@ class IntellitipCommand(sublime_plugin.TextCommand):
 
     def load_settings(self):
         self.settings = sublime.load_settings("intellitip.sublime-settings")
+        self.load_css_file_to_settings()
 
-        load_css_file_to_settings(self.settings)
+    def load_css_file_to_settings(self):
+        css_file = "Packages/" + self.settings.get("css_file", "Intellitip/css/default.css")
 
-    def run(self, _):
-        view_settings = self.view.settings()
+        css = sublime.load_resource(css_file).replace("\r", "")
 
-        if view_settings.get('is_widget'):
-            return
+        self.settings.set("css", css)
 
-        for region in self.view.sel():
-            region_row, _ = self.view.rowcol(region.begin())
+    def get_selected_function_name(self):
+        selection = self.view.sel()[0]
+        word      = self.view.word(selection)
 
-            if region_row != view_settings.get('intellitip_row', -1):
-                view_settings.set('intellitip_row', region_row)
-            else:
-                return
+        return self.view.substr(word)
 
-            # Find db for lang
-            self.get_lang(self.view)
-            print(self.lang)
+    def get_language(self):
+        language = None
 
-            if self.lang not in self.cache:
-                path_db = os.path.dirname(os.path.abspath(__file__)) + "/db/%s.json" % self.lang
-
-                if os.path.exists(path_db):
-                    self.cache[self.lang] = json.load(open(path_db))
-                else:
-                    self.cache[self.lang] = {}
-
-            completions = self.cache[self.lang]
-
-            # Find in completions
-            if completions:
-                function_names = get_function_names(self.view)
-                found = False
-
-                for function_name in function_names:
-                    completion = completions.get(function_name)
-
-                    if completion:
-                        found = completion
-                        break
-
-                if found:
-                    menus = ['<style>%s</style>' % self.settings.get("css")]
-
-                    # Syntax
-                    menus.append("<h1>%s</h1>" % found["syntax"])
-
-                    # Spit long description lines
-                    for descr in re.sub("(.{100,120}[\\.]) ", "\\1||", found["descr"]).split("||"):
-                        menus.append("<br>" + descr + "<br>")
-
-                    # Parameters
-                    if found["params"]:
-                        menus.append("<h1>Parameters:</h1>")
-
-                    for parameter in found["params"]:
-                        menus.append("- <b>" + parameter["name"] + ":</b> " + parameter["descr"] + "<br>")
-
-                    self.append_links(menus, found)
-
-                    self.view.show_popup(''.join(menus), location = -1, max_width = 600, on_navigate = self.on_navigate)
-                else:
-                    self.view.hide_popup()
-
-    def append_links(self, menus, found):
-        for pattern, _ in sorted(self.settings.get("help_links").items()):
-            if re.match(pattern, found["path"]):
-                menus.append('<br>Open docs: <a href="%s">Docs</a>' % found["name"])
-                break
-
-        return menus
-
-    def get_lang(self, view):
         # Try to match against the current scope
-        scope = view.scope_name(view.sel()[0].b)
+        scope = self.view.scope_name(self.view.sel()[0].b)
 
-        for match, lang in self.settings.get("docs").items():
+        for match, lang2 in self.settings.get("docs").items():
             if re.match(".*" + match, scope):
-                self.lang = lang
-                return
+                language = lang2
 
-        syntax = view.settings().get("syntax")
+        syntax = self.view.settings().get("syntax")
 
         # No match in predefined docs, return from syntax filename
         matched = re.match(".*/(.*?).sublime-syntax", syntax)
 
         if matched:
-            self.lang = matched.group(1)
-            return
+            language = matched.group(1)
 
         # No match in syntax filename, try tmLanguage
         matched = re.match(".*/(.*?).tmLanguage", syntax)
 
         if matched:
-            self.lang = matched.group(1)
+            language = matched.group(1)
+
+        return language
+
+    def add_completions_to_cache(self, language):
+        path_db = os.path.dirname(os.path.abspath(__file__)) + "/db/%s.json" % language
+
+        if os.path.exists(path_db):
+            completions = json.load(open(path_db))
+        else:
+            completions = {}
+
+        self.cached_completions[language] = completions
+
+    def get_completions_from_cache(self, language):
+        if language not in self.cached_completions:
+            self.add_completions_to_cache(language)
+
+        return self.cached_completions[language]
+
+    def build_completion_html(self, completion_data):
+        html_message = []
+
+        html_message.append("<style>%s</style>" % self.settings.get("css"))
+        html_message.append("<h1>%s</h1>"       % completion_data["syntax"])
+        html_message.append("<br>%s<br>"        % completion_data["descr"])
+
+        if completion_data["params"]:
+            html_message.append("<h1>Parameters:</h1>")
+
+        for parameter in completion_data["params"]:
+            html_message.append("- <b>" + parameter["name"] + ":</b> " + parameter["descr"] + "<br>")
+
+        for pattern, _ in sorted(self.settings.get("help_links").items()):
+            if re.match(pattern, completion_data["path"]):
+                html_message.append('<br>Open docs: <a href="%s">Docs</a>' % completion_data["name"])
+                break
+
+        return ''.join(html_message)
+
+    def on_navigate(self, link, language):
+        webbrowser.open_new_tab(self.settings.get("help_links")[language.lower()] % link)
+
+    def run(self, _):
+        language = self.get_language()
+        completions_for_language = self.get_completions_from_cache(language)
+
+        if not completions_for_language:
+            sublime.status_message("Could not find any completions for %s" % language)
             return
 
-        self.lang = None
+        selected_function_name  = self.get_selected_function_name()
+        completion_for_function = completions_for_language.get(selected_function_name)
 
-    def on_navigate(self, link):
-        webbrowser.open_new_tab(self.settings.get("help_links")[self.lang.lower()] % link)
+        if not completion_for_function:
+            sublime.status_message("Could not find any completions for %s :: %s" % (language, selected_function_name))
+            return
+
+        help_message = self.build_completion_html(completion_for_function)
+
+        self.view.show_popup(help_message,
+                             max_width = 1200,
+                             max_height = 1200,
+                             on_navigate = lambda link: self.on_navigate(link, language))
